63292f9c72059f9bedb2f08abe15ad5fb1a16fa4
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-07-15 13:15:41 -0700

    fix uri

--- tfu/__init__.py
+++ tfu/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.0.1.dev0"
 
 __title__ = "tfu"
 __description__ = "tensorflow utils"
-__uri__ = "https://github.com/diogo149/du"
+__uri__ = "https://github.com/diogo149/tfu"
 
 __author__ = "Diogo Almeida"
 __email__ = "diogo149@gmail.com"
