b08197f16c109b4fe18f919a58fc498bc516cd9b
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-07-15 20:21:18 -0700

    add msr and ortho init

--- tfu/inits.py
+++ tfu/inits.py
@@ -61,6 +61,73 @@ def xavier_uniform(shape, in_axes, out_axes, **_):
                                          maxval=magnitude)
 
 
+def msr_magnitude(shape, in_axes, out_axes):
+    """
+    http://arxiv.org/abs/1502.01852
+
+    NOTE: also called He init
+    """
+    # consider all non-out_axes as in_axes
+    in_axes_size = np.prod([s
+                            for dim, s in enumerate(shape)
+                            if dim not in out_axes])
+    # NOTE: this is actually sqrt(2) in the paper, but a gain of sqrt(2)
+    # is recommended for ReLUs
+    return float(np.sqrt(1.0 / in_axes_size))
+
+
+def msr_normal(shape, in_axes, out_axes, **_):
+    std = msr_magnitude(shape, in_axes, out_axes)
+    return tf.random_normal_initializer(stddev=std)
+
+
+def msr_uniform(shape, in_axes, out_axes, **_):
+    magnitude = float(np.sqrt(3)) * msr_magnitude(shape, in_axes, out_axes)
+    return tf.random_uniform_initializer(minval=-magnitude,
+                                         maxval=magnitude)
+
+
+def orthogonal(shape, in_axes, out_axes, **_):
+    """
+    http://arxiv.org/abs/1312.6120
+
+    implementation from Sander Dieleman
+    """
+    assert len(shape) >= 2
+
+    def inner(shape, dtype):
+        # consider all non-out_axes as in_axes
+        tmp_out_shape = []
+        tmp_in_shape = []
+        in_axes_size = 1
+        out_axes_size = 1
+        tmp_order = list(out_axes)
+        for dim, s in enumerate(shape):
+            if dim in out_axes:
+                out_axes_size *= s
+                tmp_out_shape.append(s)
+            else:
+                in_axes_size *= s
+                tmp_in_shape.append(s)
+                tmp_order.append(dim)
+        # calculate matrix shape
+        flat_shape = (out_axes_size, in_axes_size)
+        # make orthogonal matrix
+        a = np.random.normal(0.0, 1.0, flat_shape)
+        u, _, v = np.linalg.svd(a, full_matrices=False)
+        # pick the one with the correct shape
+        q = u if u.shape == flat_shape else v
+        # reshape to temporary shape
+        q = q.reshape(tmp_out_shape + tmp_in_shape)
+        # transpose out axes from the beginning
+        transpose_axes = [None] * len(shape)
+        for idx, dim in enumerate(tmp_order):
+            transpose_axes[dim] = idx
+        return np.transpose(q, axes=transpose_axes)
+
+    return inner
+
+
 # ############################### other inits ###############################
 
 
