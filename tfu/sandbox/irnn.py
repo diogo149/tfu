cb8065e3376d9858eaf0a882fe8facd086ebf375
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-07-31 19:23:25 -0700

    fix irnn priority

--- tfu/sandbox/irnn.py
+++ tfu/sandbox/irnn.py
@@ -19,13 +19,16 @@ def irnn_hook(nonlin=tf.nn.relu):
         return nonlin(hs.kwargs["logit"])
 
     def inner(hs):
-        hs.hooks += [
-            tfu.inits.set_weight_init(identity_init,
-                                      variable_scope=["simple_rnn", "h_to_h"]),
+        # prepend so that weight init applies after other hooks (and overwrites
+        # other weight inits)
+        hs.hooks = [
+            tfu.inits.set_weight_init(
+                identity_init,
+                variable_scope=["SimpleRNNStep", "h_to_h"]),
             tfu.filter_dsl(replace_nonlinearity,
                            key=["nonlinearity"],
-                           variable_scope=["simple_rnn"]),
-        ]
+                           variable_scope=["SimpleRNNStep"]),
+        ] + hs.hooks
         return hs()
 
     return inner
