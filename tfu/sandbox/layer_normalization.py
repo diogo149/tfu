5e850b31a3a5c13c59ee1d18225345d6a4c71e20
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-07-31 19:39:44 -0700

    add LNN-RNN

--- tfu/sandbox/layer_normalization.py
+++ tfu/sandbox/layer_normalization.py
@@ -18,3 +18,27 @@ def layer_normalization(name, tensor, epsilon=1e-4):
         z = tfu.learned_scaling("scale", z)
         z = tfu.add_bias("bias", z)
         return z
+
+
+class LNSimpleRNNStep(tfu.RNNStep):
+
+    def __init__(self, num_units):
+        self.num_units = num_units
+
+    def state_specification(self):
+        return self.num_units
+
+    def call(self, inputs, state):
+        x, = inputs
+        h = state
+        logit = tfu.add_bias(
+            "bias",
+            tfu.linear("x_to_h", x, self.num_units) +
+            tfu.linear("h_to_h", h, self.num_units))
+        logit = layer_normalization("ln", logit)
+
+        @tfu.hooked
+        def nonlinearity(logit):
+            return tf.tanh(logit)
+
+        return nonlinearity(logit=logit)
