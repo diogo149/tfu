f12d46a6352338d555b8e05d24f545308743dd4a
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-08-01 08:44:42 -0700

    add more efficient LSTM

--- tfu/rnn_steps.py
+++ tfu/rnn_steps.py
@@ -29,7 +29,12 @@ class SimpleRNNStep(RNNStep):
             return nonlinearity(logit=logit)
 
 
-class LSTMStep(RNNStep):
+class LSTMStep_v1(RNNStep):
+
+    """
+    slightly slower implementation of LSTM that still
+    may be useful to fork off of
+    """
 
     def __init__(self, num_units):
         self.num_units = num_units
@@ -58,3 +63,40 @@ class LSTMStep(RNNStep):
             new_h = tf.tanh(new_c) * o
             return {"h": new_h, "c": new_c}
 
+
+class LSTMStep(RNNStep):
+
+    def __init__(self, num_units):
+        self.num_units = num_units
+
+    def state_specification(self):
+        return {"h": self.num_units, "c": self.num_units}
+
+    def call(self, inputs, state):
+        with tf.variable_scope("lstm"):
+            x, = inputs
+            h = state["h"]
+            c = state["c"]
+
+            multi_names = ["forget", "input", "output", "update"]
+            multi_units = [self.num_units] * 4
+            with tf.variable_scope("x_to_h"):
+                x_logits = tf_utils.multi_linear(names=multi_names,
+                                                 tensor=x,
+                                                 num_units=multi_units)
+            with tf.variable_scope("h_to_h"):
+                h_logits = tf_utils.multi_linear(names=multi_names,
+                                                 tensor=h,
+                                                 num_units=multi_units)
+            logits = []
+            for name, x_logit, h_logit in zip(multi_names, x_logits, h_logits):
+                with tf.variable_scope(name):
+                    logit = tf_utils.add_bias("bias", x_logit + h_logit)
+                logits.append(logit)
+            f = tf.nn.sigmoid(logits[0])
+            i = tf.nn.sigmoid(logits[1])
+            o = tf.nn.sigmoid(logits[2])
+            u = tf.tanh(logits[3])
+            new_c = f * c + i * u
+            new_h = tf.tanh(new_c) * o
+            return {"h": new_h, "c": new_c}
