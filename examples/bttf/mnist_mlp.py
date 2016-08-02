d7be288e5600be3dc0dad875ef155abd00f428b9
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-08-02 10:53:33 -0700

    add backprop to the future implementation

---
+++ examples/bttf/mnist_mlp.py
@@ -0,0 +1,105 @@
+import numpy as np
+import tensorflow as tf
+import tfu
+from tfu.sandbox import bttf
+import du
+
+
+def bachelor_normalization(name, x, beta=0.95, epsilon=1e-4):
+    # TODO try different beta for mu and mu2
+    with tf.variable_scope(name):
+        curr_indices = filter(lambda x: x != 1,
+                              range(tfu.ndim(x)))
+        mix_indices = filter(lambda x: x != 0,
+                             curr_indices)
+
+        # shift by mean
+        current_mean = tf.reduce_mean(x,
+                                      reduction_indices=curr_indices,
+                                      keep_dims=True)
+        mix_mean = tf.reduce_mean(x,
+                                  reduction_indices=mix_indices,
+                                  keep_dims=True)
+        bttf_mu = bttf.bttf_mean(current_mean=current_mean)
+        mu = beta * bttf_mu + (1 - beta) * mix_mean
+
+        # scale by x^2
+        # TODO should we scale by x^2 or (x-mu)^2
+        if 1:
+            x2 = tf.square(x)
+        else:
+            x2 = tf.square(x - mu)
+        current_mean2 = tf.reduce_mean(x2,
+                                       reduction_indices=curr_indices,
+                                       keep_dims=True)
+        mix_mean2 = tf.reduce_mean(x2,
+                                   reduction_indices=mix_indices,
+                                   keep_dims=True)
+        bttf_mu2 = bttf.bttf_mean(current_mean=current_mean2,
+                                  mean_initializer=1,
+                                  name="bttf_mean2")
+        mu2 = beta * bttf_mu2 + (1 - beta) * mix_mean2
+
+        scale = tfu.learned_scaling("scale",
+                                    tf.inv(tf.sqrt(mu2 + epsilon)),
+                                    axis=1)
+        res = (x - mu) * scale
+        res = tfu.add_bias("bias", res, axis=1)
+        return res
+
+
+train, valid, test = du.tasks.image_tasks.mnist("float32")
+for dataset in [train, valid, test]:
+    dataset["x"] = dataset["x"].astype("float32")
+    dataset["y"] = dataset["y"].astype("int64")
+
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 1, 28, 28])
+y_ = tf.placeholder(tf.int64, shape=[None])
+
+h = tfu.flatten(x, 2)
+
+with tf.variable_scope("mlp",
+                       initializer=tf.random_uniform_initializer(-0.05, 0.05)):
+    h = tfu.linear("fc1", h, 512)
+    h = bachelor_normalization("bn1", h)
+    h = tf.nn.relu(h)
+    h = tfu.linear("fc2", h, 512)
+    h = bachelor_normalization("bn2", h)
+    h = tf.nn.relu(h)
+    h = tfu.affine("logit", h, 10)
+
+cross_entropy = tf.reduce_mean(tfu.softmax_cross_entropy_with_logits(h, y_))
+
+# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
+
+bttf_update_op = bttf.bttf_mean_updates(cross_entropy, alpha1=0.75)
+
+
+def to_minibatches(dataset, batch_size):
+    epoch_size = len(dataset.values()[0])
+    while True:
+        res = {k: [] for k in dataset}
+        for _ in range(batch_size):
+            idx = np.random.randint(epoch_size)
+            for k, v in dataset.items():
+                res[k].append(v[idx])
+        res = {k: np.array(v) for k, v in res.items()}
+        yield res
+
+accuracy = tf.reduce_mean(tfu.categorical_accuracy(h, y_))
+
+sess.run(tf.initialize_all_variables())
+
+train_gen = to_minibatches(train, 500)
+for _ in range(25):
+    with du.timer("epoch"):
+        for i in range(100):
+            batch = train_gen.next()
+            sess.run([train_step, bttf_update_op],
+                     feed_dict={x: batch["x"],
+                                y_: batch["y"]})
+        print(accuracy.eval(feed_dict={x: valid["x"],
+                                       y_: valid["y"]}))
---
+++ tfu/sandbox/bttf.py
@@ -0,0 +1,93 @@
+import collections
+import tensorflow as tf
+import tfu
+
+
+def _backprop_to_the_future_mean_impl(current_mean,
+                                      rolling_mean,
+                                      rolling_grad):
+    return rolling_mean
+
+
+@tf.RegisterGradient("BackpropToTheFutureMean")
+def _backprop_to_the_future_mean_grad(op, grad):
+    current_mean, rolling_mean, rolling_grad = op.inputs
+    return (rolling_grad,
+            # tf.zeros_like(rolling_mean),
+            None,
+            None,
+            )
+
+
+def backprop_to_the_future_mean(current_mean,
+                                rolling_mean=None,
+                                rolling_grad=None,
+                                mean_initializer=0,
+                                name="bttf_mean"):
+    with tf.variable_scope(name):
+        if rolling_mean is None:
+            rolling_mean = tfu.get_variable(
+                name="rolling_mean",
+                shape=current_mean.get_shape(),
+                dtype=current_mean.dtype,
+                initializer=tf.constant_initializer(value=mean_initializer),
+                trainable=False,
+            )
+        if rolling_grad is None:
+            rolling_grad = tfu.get_variable(
+                name="rolling_grad",
+                shape=current_mean.get_shape(),
+                dtype=current_mean.dtype,
+                initializer=tf.constant_initializer(value=0.0),
+                trainable=False,
+            )
+
+        g = tf.get_default_graph()
+        with g.gradient_override_map({"PyFuncStateless": "BackpropToTheFutureMean"}):
+            out, = tf.py_func(func=_backprop_to_the_future_mean_impl,
+                              inp=[current_mean,
+                                   rolling_mean,
+                                   rolling_grad],
+                              Tout=[current_mean.dtype],
+                              stateful=False,
+                              name=None)
+            out.set_shape(current_mean.get_shape())
+
+        tf.add_to_collection("bttf_current_mean", current_mean)
+        tf.add_to_collection("bttf_rolling_mean", rolling_mean)
+        tf.add_to_collection("bttf_rolling_grad", rolling_grad)
+        tf.add_to_collection("bttf_mean", out)
+        return out
+
+
+def backprop_to_the_future_mean_updates(loss, alpha1, alpha2=None):
+    if alpha2 is None:
+        alpha2 = alpha1
+
+    results = tf.get_collection("bttf_mean")
+    rm_to_cm = collections.defaultdict(list)
+    rg_to_res = collections.defaultdict(list)
+    for res in results:
+        current_mean, rolling_mean, rolling_grad = res.op.inputs
+        rm = tfu.tensor_to_variable(rolling_mean)
+        rg = tfu.tensor_to_variable(rolling_grad)
+        rm_to_cm[rm].append(current_mean)
+        rg_to_res[rg].append(res)
+
+    updates = []
+
+    for rm, cms in rm_to_cm.items():
+        avg_cm = tfu.list_reduce_mean(cms)
+        updates.append(rm.assign(alpha1 * rm + (1 - alpha1) * avg_cm))
+
+    res_to_grad = dict(zip(results, tf.gradients(loss, results)))
+    for rg, ress in rg_to_res.items():
+        grads = [res_to_grad[res] for res in ress]
+        avg_grad = tfu.list_reduce_mean(grads)
+        updates.append(rg.assign(alpha2 * rg + (1 - alpha2) * avg_grad))
+
+    return tf.group(*updates)
+
+# shortcuts
+bttf_mean = backprop_to_the_future_mean
+bttf_mean_updates = backprop_to_the_future_mean_updates
